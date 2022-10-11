ac61a7388100c984d4e60c8d776dd83d0be053b6
mariaprotasova <mariya.protasova.03@gmail.com>
2022-10-11 20:49:19 +0300

    - init commit

---
+++ src/main/java/io/proj3c/SpringDemoBotMary/SpringDemoBotMaryApplication.java
@@ -0,0 +1,13 @@
+package io.proj3c.SpringDemoBotMary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBotMaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoBotMaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3c/SpringDemoBotMary/SpringDemoBotMaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3c.SpringDemoBotMary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBotMaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
